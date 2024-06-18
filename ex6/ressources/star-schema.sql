4a6a3b85876ba01369624e6933c62a2507c7d0d2
efi <elisabeth.fittschen@gmail.com>
2024-06-18 15:49:44 +0200

    Update star-schema.sql
    Added commit, and made primary keys serial

--- ex6/ressources/star-schema.sql
+++ ex6/ressources/star-schema.sql
@@ -1,29 +1,29 @@
 CREATE TABLE DW_Sale (
-  ShopID int NOT NULL ,
+  ShopID int NOT NULL,
   ArticleID int NOT NULL,
-  SaleID int NOT NULL,
+  SaleID SERIAL PRIMARY KEY,
   SaleDate DATE NOT NULL,
-  PRIMARY KEY (SaleID),
   Amount int NOT NULL,
+  Revenue double precision NOT NULL
 );
 
 CREATE TABLE DW_Shop (
-  ShopID int NOT NULL ,
+  ShopID SERIAL PRIMARY KEY,
   City varchar(255) NOT NULL,
   Region varchar(255) NOT NULL,
   Country varchar(255) NOT NULL,
-  Name varchar(255) NOT NULL,
-  PRIMARY KEY (ShopID)
+  Name varchar(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE DW_Article (
-  ArticleID int NOT NULL ,
+  ArticleID SERIAL PRIMARY KEY,
   ProductGroup varchar(255) NOT NULL,
   ProductFamily varchar(255) NOT NULL,
-  Name varchar(255) NOT NULL,
-  Price double precision NOT NULL,
-  PRIMARY KEY (ArticleID)
+  Name varchar(255) NOT NULL UNIQUE,
+  Price double precision NOT NULL
 );
 
 ALTER TABLE DW_Sale ADD CONSTRAINT ShopID_fk_1 FOREIGN KEY (ShopID) REFERENCES DW_Shop (ShopID);
 ALTER TABLE DW_Sale ADD CONSTRAINT ArticleID_fk_1 FOREIGN KEY (ArticleID) REFERENCES DW_Article (ArticleID);
+
+commit;
